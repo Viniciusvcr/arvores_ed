66061f1e5f690396042cac0426f19b4471b8cc90
Vinícius Regatieri <viniciusvcr@hotmail.com>
2018-05-22 20:50:47 -0300

    Add funções aula

--- arvore_binaria_busca.cpp
+++ arvore_binaria_busca.cpp
@@ -24,3 +24,27 @@ int vazia(arvore* a){
 	return a->raiz == NULL;
 }
 
+no* cria_no(item x){
+	no *novo = (no*)malloc(sizeof(no));
+	novo->item = x;
+	novo->esq = NULL;
+	novo->dir = NULL;
+
+	return novo;
+}
+
+no* inserir_no(no* r, item x){
+	if(r == NULL)
+		r = cria_no(x);
+	else if(r->dado.chave > x.chave)
+		r->esq = inserir_no(r->esq, x);
+	else if(r->dado.chave < x.chave)
+		r->dir = inserir_no(r->dir, x);
+
+	return r;
+}
+
+void insere(arvore* a, item x){
+	a->raiz = inserir_no(a->raiz, x);
+}
+
